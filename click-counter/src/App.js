9165d3423fdbdec939c7e81fa48aadf1bca5a3fb
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-07-03 11:49:26 +1000

    feat(click-counter): add counter

--- click-counter/src/App.js
+++ click-counter/src/App.js
@@ -2,8 +2,30 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  incrementCounter = (e) => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  };
   render() {
-    return <div data-test="component-app">Hello World</div>;
+    const { counter } = this.state;
+    return (
+      <div data-test="component-app">
+        <div data-test="counter-display">
+          The counter is currently {counter}
+        </div>
+        <button
+          data-test="increment-button"
+          type="button"
+          onClick={(e) => this.incrementCounter()}
+        >
+          Add
+        </button>
+      </div>
+    );
   }
 }
 
