3b0a25a0afb3d451499e551e29c3040cedafd103
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-07-03 12:35:56 +1000

    tst(click-counter): increment button click & counter add

--- click-counter/src/App.test.js
+++ click-counter/src/App.test.js
@@ -13,7 +13,9 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}, state = null) => {
-  return shallow(<App {...props} />);
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
 };
 
 /**
@@ -42,5 +44,19 @@ test("renders counter display", () => {
   const counterDisplay = findByTestAttr(wrapper, "counter-display");
   expect(counterDisplay.length).toBe(1);
 });
-test("counter starts at 0", () => {});
-test("clicking button increments counter display", () => {});
+test("counter starts at 0", () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state("counter");
+  expect(initialCounterState).toBe(0);
+});
+test("clicking button increments counter display", () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate("click");
+
+  expect(wrapper.state("counter")).toBe(counter + 1);
+
+  const counterDisplay = findByTestAttr(wrapper, "counter-display");
+  expect(counterDisplay.text()).toContain(counter + 1);
+});
