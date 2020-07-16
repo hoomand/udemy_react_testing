bc08009479d39c73a29a21d5d70405bd91b7ccf5
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-07-16 10:14:48 +1000

    jotto: add CORRECT_GUESS action creator & tests

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -3,3 +3,7 @@ export const actionTypes = {
   GUESS_WORD: "GUESS_WORD",
   SET_SECRET_WORD: "SET_SECRET_WORD",
 };
+
+export function correctGuess() {
+  return { type: actionTypes.CORRECT_GUESS };
+}
