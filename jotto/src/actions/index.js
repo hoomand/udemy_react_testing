5c343ffb9855392677c68e55740ab53ba07b8878
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-07-20 08:27:35 +1000

    jotto: add Input test with success being false

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -7,3 +7,26 @@ export const actionTypes = {
 export function correctGuess() {
   return { type: actionTypes.CORRECT_GUESS };
 }
+
+/**
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ *     and (conditionally) CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord - Guessed word.
+ * @returns {function} - Redux Thunk function.
+ */
+export const guessWord = (guessedWord) => {
+  return function (dispatch, getState) {
+    const secretWord = getState().secretWord;
+    const letterMatchCount = getLetterMatchCount(guessedWord, secretWord);
+
+    dispatch({
+      type: actionTypes.GUESS_WORD,
+      payload: { guessedWord, letterMatchCount },
+    });
+
+    if (guessedWord === secretWord) {
+      dispatch({ type: actionTypes.CORRECT_GUESS });
+    }
+  };
+};
