40fda15ed11cbb7e84cc9d823192e379c8d6f92d
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-07-24 19:02:55 +1000

    jotto: add axios test with moxios

--- jotto/src/actions/index.js
+++ jotto/src/actions/index.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { getLetterMatchCount } from "../helpers";
 
 export const actionTypes = {
@@ -30,5 +31,21 @@ export const guessWord = (guessedWord) => {
     if (guessedWord === secretWord) {
       dispatch({ type: actionTypes.CORRECT_GUESS });
     }
+  };
+};
+
+/**
+ * Returns Redux Thunk function that initiates an axios request
+ *    and dispatches the response as a 'SET_SECRET_WORD' action
+ * @returns {function} - Redux Thunk function.
+ */
+export const getSecretWord = () => {
+  return (dispatch) => {
+    return axios.get("http://localhost:3030").then((response) => {
+      dispatch({
+        type: actionTypes.SET_SECRET_WORD,
+        payload: response.data,
+      });
+    });
   };
 };
