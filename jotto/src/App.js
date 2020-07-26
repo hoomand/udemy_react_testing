844b7dc4dd2dcb57c957e43d9d3a80c37d21bdc2
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2020-07-27 08:42:00 +1000

    jotto: test getSecretWord in App component

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -7,7 +7,10 @@ import GuessedWords from "./GuessedWords";
 import Congrats from "./Congrats";
 import Input from "./Input";
 
-class App extends Component {
+export class UnconnectedApp extends Component {
+  componentDidMount() {
+    this.props.getSecretWord();
+  }
   render() {
     const { success, guessedWords } = this.props;
     return (
@@ -26,4 +29,4 @@ const mapStateToProps = (state) => {
   return { success, guessedWords, secretWord };
 };
 
-export default connect(mapStateToProps, { getSecretWord })(App);
+export default connect(mapStateToProps, { getSecretWord })(UnconnectedApp);
